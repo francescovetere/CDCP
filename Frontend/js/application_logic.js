966022e3fb5dbe535dac5a7d16b3f48a461853a6
Francesco Vetere <francescovetere1997@gmail.com>
2020-11-24 18:24:06 +0100

    Add listeners for login/registration buttons

--- Frontend/js/application_logic.js
+++ Frontend/js/application_logic.js
@@ -1,33 +1,65 @@
+// Variabile globale che rappresenta il contenuto variabile della pagina 
+// TODO: dovrà chiaramente diventare locale, lasciarla globale è cattivo stile
 let variableContent = document.getElementById("variable-content");
 
 function createLoginPage() {
+    // Recupero l'HTML del template della form di login
     let loginHTML = document.querySelector('script#login-form-template').textContent;
-    variableContent.innerHTML = loginHTML;
 
-    // Se clicco su "New to CDCP? Sign in!", carico la pagina di login
+    // Cliccando su "New to CDCP? Sign in!", viene richiamata la funzione per la costruzione della pagina di login
     document.getElementById("sign-in-redirect").addEventListener("click", createRegistrationPage);
+
+    // Cliccando su "Login", viene fatta la chiamata AJAX al server (validando prima i campi)
+    // Se il login va a buon fine, si viene redirezionati alla pagina dei progetti, che funge da home page per l'utente
+    document.getElementById("btn-login-id").addEventListener("click",
+        function() {
+            // TODO
+            // - validazione parametri
+            // - chiamata AJAX al server
+            // - se risposta positiva: createProjectsPage();
+        }
+    );
+
+    // Inserisco l'HTML recuperato nel div che rappresenta il contenuto variabile
+    variableContent.innerHTML = loginHTML;
 }
 
 function createRegistrationPage() {
+    // Recupero l'HTML del template della form di registrazione
     let registrationHTML = document.querySelector('script#registration-form-template').textContent;
+
+    // Cliccando su "Sign in", viene fatta la chiamata AJAX al server (validando prima i campi)
+    // Se la registrazione va a buon fine, si viene redirezionati alla pagina di login
+    document.getElementById("btn-login-id").addEventListener("click",
+        function() {
+            // TODO
+            // - validazione parametri
+            // - chiamata AJAX al server
+            // - se risposta positiva: createLoginPage();
+        }
+    );
+
+    // Inserisco l'HTML recuperato nel div che rappresenta il contenuto variabile
     variableContent.innerHTML = registrationHTML;
 }
 
 function createProjectsPage() {
-    // Creo il div esterno che conterrà tutti i progetti
-    let externalProjectDiv = document.createElement("div");
-    externalProjectDiv.setAttribute("class", "row");
-    externalProjectDiv.setAttribute("id", "cardContent");
-    
     // Recupero l'HTML del template della project card
     let projectCardHTML = document.querySelector('script#project-card-template').textContent;
+    // TODO: Aggiungere all'elemento i vari listener dove servono
+
+    // La card non andrà inserita direttamente nel div variabile:
+    // Creo iinfatti un div esterno che conterrà tutte le card dei progetti
+    let projectsDiv = document.createElement("div");
+    projectsDiv.setAttribute("class", "row");
+    projectsDiv.setAttribute("id", "external-project-div")
     
     // Inserisco nel div appena creato l'HTML di 9 project card di esempio
     for(let i = 0; i < 9; ++i)
-        externalProjectDiv.innerHTML += projectCardHTML;
+        projectsDiv.innerHTML += projectCardHTML;
 
     // Appendo al div principale della pagina il nuovo div contenente tutti i progetti
-    variableContent.appendChild(externalProjectDiv);
+    variableContent.appendChild(projectsDiv);
 }
 
 function createLogsPage() {
@@ -38,6 +70,15 @@ function createStatsPage() {
     //TODO
 }
 
-// Logica di creazione delle pagine
+
+
+/****************************************/
+/****************************************/
+/******** LOGICA DI NAVIGAZIONE *********/ 
+/****************************************/
+/****************************************/
+
+// Per il momento, commentare e decommentare per effettuare i test sulle varie pagine
+
 // createLoginPage();
 createProjectsPage();
