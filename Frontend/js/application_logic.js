fb708bff427d967dfb4d47c6ec5f8e29a1642ea3
Giuseppe La Gualano <info.giuseppelagualano@gmail.com>
2020-11-28 22:42:35 +0100

    add back-to-top, modal delete and navbar nickname
    Use JQuery

--- Frontend/js/application_logic.js
+++ Frontend/js/application_logic.js
@@ -49,17 +49,45 @@ function createProjectsPage() {
     // TODO: Aggiungere all'elemento i vari listener dove servono
 
     // La card non andrà inserita direttamente nel div variabile:
-    // Creo iinfatti un div esterno che conterrà tutte le card dei progetti
+    // Creo infatti un div esterno che conterrà tutte le card dei progetti
     let projectsDiv = document.createElement("div");
     projectsDiv.setAttribute("class", "row");
-    projectsDiv.setAttribute("id", "external-project-div")
+    projectsDiv.setAttribute("id", "external-project-div");
     
+ 
     // Inserisco nel div appena creato l'HTML di 9 project card di esempio
-    for(let i = 0; i < 9; ++i)
+    for(let i = 0; i < 19; ++i){
         projectsDiv.innerHTML += projectCardHTML;
+        //deleteProj[i].id = "prova-delete-" + i;
+    }
 
     // Appendo al div principale della pagina il nuovo div contenente tutti i progetti
     variableContent.appendChild(projectsDiv);
+
+    // Blocco gestito con JQuery ma da riadattare in altro modo. Questo è solo a scopo dimostrativo per prendere esempio
+    let idCount = 1;
+    $('#external-project-div .IDProjectTitle').each(function() {
+        $(this).html("#ID-"+idCount);
+        idCount++;
+    });
+
+    idCount = 1;
+    $('#external-project-div #delete-').each(function() {
+        $(this).attr('id', 'delete-' + idCount);
+        idCount++;
+    });
+
+    idCount = 1;
+    $('#external-project-div .IDProjectContent').each(function() {
+        $(this).html("prova contenuto di #ID-"+idCount);
+        idCount++;
+    });
+
+    
+
+    // Crea finestra modale per eliminazione
+    let deleteModalHTML = document.querySelector('script#delete-card-Modal').textContent;
+    projectsDiv.innerHTML += deleteModalHTML;
 }
 
 function createLogsPage() {
@@ -70,7 +98,29 @@ function createStatsPage() {
     //TODO
 }
 
+function removeProject(id){
+    let ret_id = id.replace('delete-','');
+    $(document).ready(function(){
+        $(".deleteBody").html("Sei sicuro di voler eliminare il progetto #ID-"+ret_id+" ?");
+      });
+    $('#deleteModal').modal('show');
+}
 
+// Create Scroll to top in all page
+$(document).ready(function(){
+    $('body').append('<div id="toTop" class="btn btn-info"><span class="fas fa-arrow-up"></span> Back to Top</div>');
+      $(window).scroll(function () {
+          if ($(this).scrollTop() != 0) {
+              $('#toTop').fadeIn();
+          } else {
+              $('#toTop').fadeOut();
+          }
+      }); 
+  $('#toTop').click(function(){
+      $("html, body").animate({ scrollTop: 0 }, 600);
+      return false;
+  });
+});
 
 /****************************************/
 /****************************************/
