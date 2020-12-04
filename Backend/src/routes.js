ad21c8d66355f6941a4ffade877c8341f7b0a40f
Francesco Vetere <francescovetere1997@gmail.com>
2020-12-04 16:03:36 +0100

    Refactor DB, update API

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -4,21 +4,12 @@ const DBManager = require("./DBManager");
 let dbm = new DBManager();
 const uuid = require('uuid').v4;
 
-// function sequencer() {
-//     let i = 1;
-//     return function () {
-//         const n = i;
-//         i++;
-//         return n;
-//     }
-// }
-
-// const seq = sequencer();
-
-// // Memorizzo un array di oggetti Project, aggiornato ad ogni query in modo opportuno
-// // (utile più che altro per debug lato server)
-// let projects = [];
-
+// Esempio di chiamata POST per l'inserimento di un example in un progetto esistente
+// > http POST localhost:8000/project/9deb6ee5-e401-4d8f-9796-feeae2d63d69 'inputType=text' 'inputValue=lorem ipsum' 
+//  'tags=[
+//          {"tagName": "animals", "tagValues": ["mammals", "vertebrates"]},
+//          {"tagName": "colors", "tagValues": ["white", "black"]}
+//        ]'
 function routes(app) {
 
     /******************
@@ -27,10 +18,11 @@ function routes(app) {
  
 
     /**
-    * Registrazione di un nuovo utente
+    * Registrazione utente
     * Parametri: vuoto
     * Body: nickname, email, password
-    * Risposta: success/error
+    * Risposta positiva: success
+    * Risposta negativa: error
     */
     app.post('/sign-up', async (req, resp) => {
         console.log("Registrating user");
@@ -70,10 +62,11 @@ function routes(app) {
 
     
     /**
-    * Login di un utente esistente
+    * Login utente
     * Parametri: vuoto
     * Body: nickname, password
-    * Risposta: success/error
+    * Risposta positiva: success
+    * Risposta negativa: error
     */
     app.post('/login', async (req, resp) => {
         console.log("Loggin in user");
@@ -93,7 +86,7 @@ function routes(app) {
             console.log(err);
         }
         
-        if(result.length == 0) {
+        if(result.length === 0) {
             console.log("Login failed\n");
             resp.status(401);
             resp.json({error: "Login failed"});
@@ -115,7 +108,8 @@ function routes(app) {
      * Restituzione di tutti i progetti
      * Parametri: vuoto
      * Body: vuoto
-     * Risposta: Tutti i progetti
+     * Risposta positiva: Tutti i progetti
+     * Risposta negativa: nessuna
      */
     app.get('/projects', async (req, resp) => {
         console.log("Retrieving all projects");
@@ -138,6 +132,7 @@ function routes(app) {
         }
 
         // Invio il numero totale dei progetti, e l'array dei progetti stessi
+        resp.status(200);
         resp.json({
             total: projects.length,
             results: projects
@@ -150,7 +145,8 @@ function routes(app) {
      * Inserimento di un nuovo progetto
      * Parametri: vuoto
      * Body: title, inputType
-     * Risposta: success/error
+     * Risposta positiva: success
+     * Risposta negativa: error
      */
     app.post('/project', async (req, resp) => {
         console.log("Inserting new project");
@@ -191,7 +187,8 @@ function routes(app) {
      * Aggiornamento di un progetto esistente
      * Parametri: id progetto
      * Body: title, inputType
-     * Risposta: success/error
+     * Risposta positiva: success
+     * Risposta negativa: error
      */
     app.put('/project/:id', async (req, resp) => {
         console.log("Updating project");
@@ -240,7 +237,8 @@ function routes(app) {
      * Rimozione di un progetto esistente
      * Parametri: id progetto
      * Body: vuoto
-     * Risposta: success/error
+     * Risposta positiva: success
+     * Risposta negativa: error
      */
     app.delete('/project/:id', async (req, resp) => {
         console.log("Deleting project");
@@ -276,38 +274,162 @@ function routes(app) {
 
 
 
-    /*************************
-     * PROJECT RECORDS ROUTES 
-     *************************/
+    /**********************************
+     * PROJECT RECORDS/EXAMPLES ROUTES 
+     **********************************/
 
     /**
      * Restituzione di tutti i record di un progetto
      * Parametri: id progetto
      * Body: vuoto
-     * Risposta: Tutti i record del progetto
+     * Risposta positiva: Tutti gli examples del progetto
+     * Risposta negativa: error
      */
-    app.get('/project/:projectId', (req, resp) => {
-        // console.log(req.params.projectId + ", " + req.params.recordId);
-        resp.status(418);
-        resp.json("I'm a teapot");
+    app.get('/project/:projectId', async (req, resp) => {
+        let projectId = req.params.projectId;
+        console.log("Retrieving all examples of project with id=" + projectId);
+        
+        // Verifico l'esistenza del progetto
+        let queryResult;
+        try {
+            let sql = 'SELECT * FROM Projects WHERE id = ?';
+            let params = [projectId];
+            queryResult = await dbm.execQuery(sql, params);
+        }
+
+        catch(err) {
+            console.log(err);
+        }
+
+        // Se il progetto non esiste, viene restituito un errore
+        if(queryResult.length === 0) {
+            console.log("Project with id=" + projectId + " not found\n");
+            resp.status(404);
+            resp.json({error: "Project with id=" + projectId + " not found"});
+            return;
+        }
+
+        // In caso il progetto esista, recupero tutti i suoi examples
+        try {
+            let sql = 'SELECT * FROM Examples WHERE projectId = ?';
+            let params = [projectId];
+            queryResult = await dbm.execQuery(sql, params);
+        }
+
+        catch(err) {
+            console.log(err);
+        }
+        
+        // Ciclo sull'array risultato e costruisco l'array di risposta
+        let examples = [];
+        for(let i = 0; i < queryResult.length; ++i) {
+            examples.push(queryResult[i]);
+        }
+
+        // Invio il numero totale dei record, e l'array dei record stessi
+        resp.status(200);
+        resp.json({
+            total: examples.length,
+            results: examples
+        });
+
+        console.log("Examples of project with id="+ projectId + " retrieved correctly\n");
     });
 
     /**
      * Inserimento di un nuovo record nel progetto
      * Parametri: id progetto
-     * Body: TODO
-     * Risposta: il record appena inserito
+     * Body: inputType, inputValue, tags
+     * Nota bene: tags è un campo composto in questo modo
+     * tags: [
+     *          {"tagName": "animals", "tagValues": ["mammals", "vertebrates"]},
+     *          {"tagName": "colors", "tagValues": ["white", "black"]}
+     *       ]
+     * Risposta positiva: success
+     * Risposta negativa: error
      */
-    app.post('/project/:projectId', (req, resp) => {
-        resp.status(418);
-        resp.json("I'm a teapot");
+    app.post('/project/:projectId', async (req, resp) => {
+        let projectId = req.params.projectId;
+        console.log("Inserting new example in project with id=" + projectId);
+
+        let inputType = req.body.inputType;
+        let inputValue = req.body.inputValue;
+        let tags = JSON.parse(req.body.tags);
+
+        // Validazione campi body
+        // Controllo sull'inputType
+        if(inputType != "text" && inputType != "image") {
+            console.log("inputType must be 'text' or 'image'\n");
+
+            resp.status(400);
+            resp.json({error: "inputType must be 'text' or 'image'"});
+            return;
+        }
+
+        // Verifico l'esistenza del progetto
+        let queryResult;
+        try {
+            let sql = 'SELECT * FROM Projects WHERE id = ?';
+            let params = [projectId];
+            queryResult = await dbm.execQuery(sql, params);
+        }
+ 
+        catch(err) {
+            console.log(err);
+        }
+
+        // Se il progetto non esiste, viene restituito un errore
+        if(queryResult.length === 0) {
+            console.log("Project with id=" + projectId + " not found\n");
+            resp.status(404);
+            resp.json({error: "Project with id=" + projectId + " not found"});
+            return;
+        }
+
+        // In caso il progetto esista, inserisco il nuovo example
+        let exampleId = uuid();
+        try {
+            let sql = `INSERT INTO Examples(id, projectId, inputType, inputValue) VALUES (?, ?, ?, ?)`
+
+            let params = [exampleId, projectId, inputType, inputValue];
+            await dbm.execQuery(sql, params);
+        }
+
+        catch(err) {
+            console.log(err);
+        }
+
+        // Inserisco tagNames e tagValues presenti eventualmente presenti nel campo tags, nelle rispettive tabelle
+        for(let i = 0; i < tags.length; ++i) {
+            let tagName = tags[i].tagName;
+            // console.log(tagName);
+            
+            try {
+                let sql = `INSERT INTO Examples(id, projectId, inputType, inputValue) VALUES (?, ?, ?, ?)`
+    
+                let params = [exampleId, projectId, inputType, inputValue];
+                await dbm.execQuery(sql, params);
+            }
+    
+            catch(err) {
+                console.log(err);
+            }
+        }
+
+
+        // console.log(tags[0].tagValues[1]);
+        console.log("Example inserted correctly\n");
+
+        resp.status(201);
+        resp.json({success: "Example inserted correctly"});
     });
 
     /**
      * Aggiornamento di un record esistente nel progetto
      * Parametri: id progetto, id record
      * Body: TODO
-     * Risposta: il record appena aggiornato
+     * Risposta positiva: success
+     * Risposta negativa: error
      */
     app.put('/project/:projectId/:recordId', (req, resp) => {
         resp.status(418);
@@ -318,7 +440,8 @@ function routes(app) {
      * Rimozione di un record esistente nel progetto
      * Parametri: id progetto, id record
      * Body: vuoto
-     * Risposta: il record appena eliminato
+     * Risposta positiva: success
+     * Risposta negativa: error
      */
     app.delete('/project/:projectId/:recordId', (req, resp) => {
         resp.status(418);
