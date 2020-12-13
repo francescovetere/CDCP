d4829b41c125bf2fdac5bbd9ac0354b978070f2b
GiuseppeLG <info.giuseppelagualano@gmail.com>
2020-12-13 17:58:21 +0000

    Insert confirm password check and fix alert message

--- Frontend/js/registration_page.js
+++ Frontend/js/registration_page.js
@@ -10,6 +10,13 @@ function createRegistrationPage() {
     // Cliccando su "Alredy have an account? Login!", viene richiamata la funzione per la costruzione della pagina di login
     document.getElementById("login-redirect").addEventListener("click", createLoginPage);
     
+
+    // Reset password custom validity message
+    let passValidity  = document.querySelector("#id-conf-password");
+    passValidity.oninput = (event) => {
+        event.target.setCustomValidity('');
+    }
+
     // Cliccando su "Sign in", viene fatta la chiamata AJAX al server (validando prima i campi)
     // Se la registrazione va a buon fine, si viene redirezionati alla pagina di login
     document.getElementById("btn-registration-id").addEventListener("click",
@@ -18,23 +25,30 @@ function createRegistrationPage() {
             let username = $("#id-nickname").val();
             let mail = $("#id-email").val();
             let pwd = $("#id-password").val();
+            let confPwd = document.getElementById("id-conf-password");
 
-            // AJAX con JQuery
-            let formData = {nickname : username, email: mail, password: pwd};
-            $.ajax({
-                url: 'api/sign-up',
-                type: 'POST',
-                data : JSON.stringify(formData),
-                contentType: 'application/json',
-                success: function(response){
-                    alert("Registrazione avvenuta con successo!"); // solo per debug adesso
-                    variableContent.innerHTML = "";
-                    createLoginPage();
-                },
-                error: function(error){
-                    alert("Something went wrong...");
-                }
-            });
+            if(pwd != confPwd.value){
+                confPwd.setCustomValidity("Password doesn't match!");
+            }else{
+                confPwd.setCustomValidity("");
+                
+                // AJAX con JQuery
+                let formData = {nickname : username, email: mail, password: pwd};
+                $.ajax({
+                    url: 'api/sign-up',
+                    type: 'POST',
+                    data : JSON.stringify(formData),
+                    contentType: 'application/json',
+                    success: function(response){
+                        alert("Registrazione avvenuta con successo!"); // solo per debug adesso
+                        variableContent.innerHTML = "";
+                        createLoginPage();
+                    },
+                    error: function(error){
+                        alert("Nickname già esistente!");
+                    }
+                });
+            }
 
         }
     );
